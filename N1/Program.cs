481f91cc271deff56f2ee2b429224ef75b370aba
Алексей Викулин <vaa@megamade.ru>
2022-07-27 16:33:28 +0300

    Add project files.

---
+++ N1/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace N1 //Выберите любую папку на своем компьютере, имеющую вложенные директории. Выведите на консоль ее содержимое и содержимое всех подкаталогов.
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Выберите путь папки");
+            string path = Console.ReadLine();
+            string[] files = Directory.GetFiles(path, "*.*", SearchOption.AllDirectories);
+            foreach (string file in files)
+            {
+                Console.WriteLine(file);
+            }
+            Console.ReadKey();
+        }
+    }
+}
---
+++ N2/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace N2 //Программно создайте текстовый файл и запишите в него 10 случайных чисел. Затем программно откройте созданный файл, рассчитайте сумму чисел в нем, ответ выведите на консоль.
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string path = @"C:\Temp\Task02.txt ";
+            Random rand = new Random();
+            using (StreamWriter sw = new StreamWriter(path, false)) //создание файла с 10 числами
+            {
+                for (int i = 0; i < 10; i++)
+                {
+                    sw.WriteLine(rand.Next(1,10));
+                }
+                Console.WriteLine(rand);
+            }
+            int result = 0;
+            using (StreamReader sr = new StreamReader(path))
+            {
+                result += Convert.ToInt32(sr.ReadLine());
+            }
+            Console.WriteLine(result);
+            Console.ReadKey();
+                      
+
+        }
+    }
+}
